6b88a4c7f07399f02a908e2a14845c761b934105
= <kristoffer@kristofferhebert.com>
2015-08-27 05:37:29 -0700

    removing node_modules

---
+++ test/todoapp.js
@@ -0,0 +1,6 @@
+//http://simonsmith.io/unit-testing-react-components-without-a-dom/
+
+import ReactUtils from 'react/addons'
+const TestUtils = ReactUtils.addons.TestUtils
+
+import TodoApp from '../src/todoapp'
